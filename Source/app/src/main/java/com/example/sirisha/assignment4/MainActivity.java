eeab9f36fc14fb3c7ffc5d871451447197885989
msirisha127 <msirisha127@gmail.com>
2017-02-15 23:31:25 -0600

    commit

---
+++ Source/app/src/main/java/com/example/sirisha/assignment4/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.sirisha.assignment4;
+
+/**
+ * Created by Sirisha on 2/15/17.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+
+public class MainActivity extends Activity {
+
+    TextToSpeech ttsobject;
+    int result;
+    EditText et;
+    String text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String username = getIntent().getStringExtra("Username");
+
+        TextView tv = (TextView)findViewById(R.id.TVusername);
+        tv.setText(username);
+
+        et = (EditText)findViewById(R.id.editText);
+
+        ttsobject = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+
+                if(status == TextToSpeech.SUCCESS){
+
+                    result = ttsobject.setLanguage(Locale.UK);
+
+                }else {
+
+                    Toast.makeText(getApplicationContext(), "Feature not Supported in your Device", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void doSomething(View v) {
+
+        switch (v.getId()){
+            case R.id.bspeak:
+
+                if(result == TextToSpeech.LANG_NOT_SUPPORTED || result == TextToSpeech.LANG_MISSING_DATA){
+
+                    Toast.makeText(getApplicationContext(), "Feature not Supported in your Device", Toast.LENGTH_SHORT).show();
+                } else {
+                    text = et.getText().toString();
+
+                    ttsobject.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+
+                }
+
+                break;
+            case R.id.bstopspeaking:
+
+                if(ttsobject != null) {
+                    ttsobject.stop();
+                }
+
+
+                break;
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(ttsobject != null) {
+            ttsobject.stop();
+            ttsobject.shutdown();
+        }
+    }
+    public void onBack(View v){
+        Intent i = new Intent(MainActivity.this, LoginActivity.class);
+       
+        startActivity(i);
+    }
+}
+
+
---
+++ Source/app/src/main/java/com/example/sirisha/assignment4/RegisterActivity.java
@@ -0,0 +1,59 @@
+package com.example.sirisha.assignment4;
+
+/**
+ * Created by Sirisha on 2/15/17.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+//import com.google.android.gms.appindexing.AppIndex;
+//import com.google.android.gms.common.api.GoogleApiClient;
+
+
+public class RegisterActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+
+    public void onSignUpClick(View v)
+    {
+        if(v.getId() == R.id.Bsignupbutton)
+        {
+            EditText name = (EditText)findViewById(R.id.TFname);
+            EditText email = (EditText)findViewById(R.id.TFemail);
+            EditText uname = (EditText)findViewById(R.id.TFuname);
+            EditText pass1 = (EditText)findViewById(R.id.TFpass1);
+            EditText pass2 = (EditText)findViewById(R.id.TFpass2);
+
+            String namestr =name.getText().toString();
+            String emailstr =email.getText().toString();
+            String unamestr =uname.getText().toString();
+            String pass1str =pass1.getText().toString();
+            String pass2str =pass2.getText().toString();
+
+            if(!pass1str.endsWith(pass2str))
+            {
+                //pop msg
+                Toast pass = Toast.makeText(RegisterActivity.this, "Password doen't match!", Toast.LENGTH_SHORT);
+                pass.show();
+            }
+
+        }
+    }
+    public void onBack(View v){
+        Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+
+        startActivity(i);
+    }
+
+}
+
